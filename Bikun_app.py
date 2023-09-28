ed65c13572b28d2736877da22b194bda078c089b
Aakif52 <abubakaraakif23@gmail.com>
2023-09-28 11:33:56 +0700

    'Read and Write to Firestore, Securely'

--- Bikun_app.py
+++ Bikun_app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+from google.cloud import firestore
+import json
+
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-reddit")
+
+doc_ref = db.collection("Bikun").document("e52tYFjerfyvXiRk6AIm")
+
+doc = doc_ref.get()
 
 st.header('Bikun Detector')
+
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
