0658808337cd084c8e4467f097ec5d6718236358
Aakif52 <abubakaraakif23@gmail.com>
2023-09-28 17:53:50 +0700

    Fix some bugs

--- Bikun_app.py
+++ Bikun_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from google.cloud import firestore
+from google.oauth2 import service_account
 import json
 
+# db = firestore.Client.from_service_account_json("firestore-key.json.json")
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-reddit")
+db = firestore.Client(credentials=creds)
 
 doc_ref = db.collection("Bikun").document("e52tYFjerfyvXiRk6AIm")
 
