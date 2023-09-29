b67357f6042ea13e433bc5d90d42d4a0158e4fee
Aakif52 <abubakaraakif23@gmail.com>
2023-09-29 21:34:42 +0700

    Added Real Time Elements

--- Bikun_app.py
+++ Bikun_app.py
@@ -4,28 +4,41 @@ from streamlit_autorefresh import st_autorefresh
 from google.cloud import firestore
 from google.oauth2 import service_account
 
+import threading
+from queue import Queue
+
 import json
+
+class StringVar:
+    BIKUN_COL = "Bikun"
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds)
+cb_done = threading.Event()
+q = Queue()
 
-doc_ref = db.collection("Bikun").document("e52tYFjerfyvXiRk6AIm")
-doc_ref2 = db.collection("Keramaian").document("KsTOlpqVJmt2bmrDRlsv")
+doc_ref = db.collection(StringVar.BIKUN_COL).document("e52tYFjerfyvXiRk6AIm")
 
-st_autorefresh(interval = 60 * 1000, key="dataframerefresh")
+def on_snapshot(doc_snapshot, changes, read_time):
+    print(f"Listener triggered!")
+    q.put(doc_snapshot[0])
+    cb_done.set()
 
-doc = doc_ref.get()
-doc2 = doc_ref2.get()
 
-st.header('Bikun Detector')
+doc_watch = doc_ref.on_snapshot(on_snapshot)
 
-st.subheader(f"Keberadaan Bikun")
+placeholder = st.empty()
+while True:
+    data = q.get()
+    print(data.to_dict())
+    placeholder.empty()
 
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
-
-st.subheader(f"Keramaian Halte")
-
-st.write("The id is: ", doc2.id)
-st.write("The contents are: ", doc2.to_dict())
+    with placeholder.container():
+        bikun_state = "Ada bikun" if data.to_dict()["Ada Bikun"] else "Gak ada bikun"
+        keramaian_state = "Ramai" if data.to_dict()["Ramai"] else "Tidak ramai"
+        st.header('Bikun Detector')
+        st.subheader(f"Keberadaan Bikun")
+        st.write(bikun_state)
+        st.subheader(f"Keramaian Halte")
+        st.write(keramaian_state)
