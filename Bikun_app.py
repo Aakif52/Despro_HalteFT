fe9de0993213539bc532ba5765d78ad89af84f21
Aakif52 <abubakaraakif23@gmail.com>
2023-11-19 15:57:21 +0700

    Added Bikun Detection & Crowd Count Over Time

--- Bikun_app.py
+++ Bikun_app.py
@@ -9,13 +9,12 @@ import threading
 from queue import Queue
 from time import sleep
 
+import pandas as pd
 import json
 
 class StringVar:
     DESPRO_COL = "test_collection"
     DISPLAY_DOC = "data"
-    BIKUN_DOC = ""
-    RAMAI_DOC = ""
 
 NUM_QUERY = 10
 
@@ -59,7 +58,8 @@ def last_log_listener(col_snapshot, changes, read_time):
     cb_done.set()
 
 last_log_watch = last_log_query.on_snapshot(last_log_listener)
-# arr = =[
+
+# arr = [
 #     {
 #         'Bikun': False,
 #         'Ramai': False
@@ -73,12 +73,9 @@ last_log_watch = last_log_query.on_snapshot(last_log_listener)
 #     },
 # ]
 
-# x = []
-# y = []
 
-# for e in arr:
-#     x.append(e.get('timestamp'))
-#     y.append(e.get('Bikun'))
+
+
 
 placeholder = st.empty()
 while True:
@@ -86,15 +83,85 @@ while True:
     # print(data.to_dict())
 
     last_log = q_last_log.get()
-    print(q_logs_arr)
-    # print(last_log)
+    # print(q_logs_arr[1].get('Ramai'))
+    # x = []
+    # y1 = []
+    # y2 = []
+
+    # for e in q_logs_arr:
+    #     x.append(e.get('timestamp'))
+    #     y1.append(e.get('Bikun'))
+    #     y2.append(e.get('Ramai'))
+
+    # print(x)
+    # print(y)
+
+    chart_data = pd.DataFrame(q_logs_arr)
+
     placeholder.empty()
 
     with placeholder.container():
         bikun_state = "Ada bikun" if last_log.get("Bikun") else "Gak ada bikun"
-        keramaian_state = "Ramai" if last_log("Ramai") else "Tidak ramai"
+        keramaian_state = "Ramai" if last_log.get("Ramai") else "Tidak ramai"
         st.header('Bikun Detector')
         st.subheader(f"Keberadaan Bikun")
         st.write(bikun_state)
         st.subheader(f"Keramaian Halte")
         st.write(keramaian_state)
+        # st.subheader(f"Waktu Update Terakhir")
+        # st.write(last_log.get("timestamp"))
+
+        st.vega_lite_chart(chart_data,{
+            "mark": {"type": "bar"},
+            "encoding": {
+                "color": {
+                    "field": 'Bikun',
+                    "legend": {"titlePadding": 5, "offset": 5, "orient": "top"},
+                    "scale": {"range": ["#FF0000", "#0000FF"]},
+                    "title": "Kehadiran Bikun",
+                    "type": "nominal"
+                },
+                "tooltip": [
+                    {"field": "timestamp","timeUnit": "yearmonthdatehoursminutes", "type": "temporal"},
+                    {
+                        "field": 'Bikun',
+                        "title": "value",
+                        "type": "quantitative"
+                    },
+                    {
+                        "field": 'Bikun',
+                        "title": "color",
+                        "type": "nominal"
+                    }
+                ],
+                "x": {"timeUnit": "hoursminutes", "field": "timestamp", "scale": {"type": "time"}},
+                "y": {"axis": {"grid": "true"}, "field": 'Bikun', "scale": {}, "title": "Bikun", "type": "nominal"}
+            },
+        },)
+        st.vega_lite_chart(chart_data,{
+            "mark": {"type": "bar"},
+            "encoding": {
+                "color": {
+                    "field": 'Ramai',
+                    "legend": {"titlePadding": 5, "offset": 5, "orient": "top"},
+                    "scale": {"range": ["#FF0000", "#0000FF"]},
+                    "title": "Keramaian Halte",
+                    "type": "nominal"
+                },
+                "tooltip": [
+                    {"field": "timestamp","timeUnit": "yearmonthdatehoursminutes", "type": "temporal"},
+                    {
+                        "field": 'Ramai',
+                        "title": "value",
+                        "type": "quantitative"
+                    },
+                    {
+                        "field": 'Ramai',
+                        "title": "color",
+                        "type": "nominal"
+                    }
+                ],
+                "x": {"timeUnit": "hoursminutes", "field": "timestamp", "scale": {"type": "time"}},
+                "y": {"axis": {"grid": "true"}, "field": 'Ramai', "scale": {}, "title": "Ramai", "type": "nominal"}
+            },
+        },)
