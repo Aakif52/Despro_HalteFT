b913b14fe3ee3c3f6204ddf347c82b322dfdb23e
Aakif52 <abubakaraakif23@gmail.com>
2023-12-04 13:00:10 +0700

    Updated Timestamp

--- Bikun_app.py
+++ Bikun_app.py
@@ -108,8 +108,8 @@ while True:
         st.write(bikun_state)
         st.subheader(f"Keramaian Halte")
         st.write(keramaian_state)
-        # st.write(f"Waktu Update Terakhir")
-        # st.write(last_log.get("timestamp"))
+        st.write(f"Waktu Update Terakhir")
+        st.write(last_log.get("timestamp"))
 
         st.vega_lite_chart(chart_data,{
             "mark": {"type": "bar"},
